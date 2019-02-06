0ddf9f22ca00cf92d8e3a64e41b88604cef1332b
Jessie Jerome Tiquis <jestah6583@gmail.com>
2019-02-06 15:00:23 +0800

    Add project files.

---
+++ ELearningV1/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ELearningV1.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ELearningV1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ELearningV1.WebApiApplication" Language="C#" %>
