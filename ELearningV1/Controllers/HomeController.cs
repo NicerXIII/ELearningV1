3139b9ad8d2f2cf6c69b7bc2bd92f96810de700d
Jessie Jerome Tiquis <jestah6583@gmail.com>
2019-02-06 15:53:56 +0800

    Test

--- ELearningV1/Controllers/HomeController.cs
+++ ELearningV1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace ELearningV1.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "Home Page Test";
 
             return View();
         }
