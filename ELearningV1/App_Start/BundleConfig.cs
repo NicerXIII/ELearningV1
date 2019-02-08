4a231d04c26db58fed702cc4c77afcaea905a7ad
Jessie Jerome Tiquis <jestah6583@gmail.com>
2019-02-08 17:40:55 +0800

    Setup Nearly DONE

--- ELearningV1/App_Start/BundleConfig.cs
+++ ELearningV1/App_Start/BundleConfig.cs
@@ -8,8 +8,21 @@ namespace ELearningV1
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/font-awesome.min.css"));
+
+            bundles.Add(new StyleBundle("~/admin-lte/css/css").Include(
+                    "~/admin-lte/css/AdminLTE.min.css",
+                    "~/admin-lte/css/skins/_all-skins.min.css"));
+
+
+            bundles.Add(new StyleBundle("~/Ionicons/css/css").Include(
+                    "~/Ionicons/css/ionicons.min.css"));
+
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                       "~/Scripts/jquery-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -17,11 +30,11 @@ namespace ELearningV1
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.min.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/admin-lte/js/adminlte.min.js").Include(
+                    "~/admin-lte/js/adminlte.min.js"));
+
         }
     }
 }
