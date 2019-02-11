8da9df0548539f3b6582a3c0b8b16d65e68522e7
Jessie Jerome Tiquis <jestah6583@gmail.com>
2019-02-11 17:19:49 +0800

    Change home comments

--- ELearningV1/Controllers/HomeController.cs
+++ ELearningV1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace ELearningV1.Controllers
     {
         public ActionResult Index()
         {
-            //This is just a test
+            //Nice ONE BROTHER
             ViewBag.Title = "Home Page Test12";
 
             return View();
