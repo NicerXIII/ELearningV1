8214750117a890c98f53c02fc7dc255d58769dd9
Chok Templonuevo <choktemplonuev10@gmail.com>
2019-02-12 15:02:55 +0800

    Exam UI

---
+++ ELearningV1/Controllers/ExamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ELearningV1.Controllers
+{
+    public class ExamController : Controller
+    {
+        // GET: Exam
+        public ActionResult Exam()
+        {
+            return View();
+        }
+    }
+}
